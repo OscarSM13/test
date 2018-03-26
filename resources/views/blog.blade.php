83ad5dd7287a797592aafc96e387b745bd0a85c5
Oscar Sitges <oscar.ss.mm@gmail.com>
2018-03-26 15:19:18 +0200

    Add view blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,3 +1,336 @@
 @extends('layouts.web')
 
 @section('title', 'Contact')
+@section('content')
+<div class="clearfix"></div>
+
+<div class="page_title2">
+<div class="container">
+
+    <h1>With Medium Image</h1>
+    <div class="pagenation">&nbsp;<a href="index.html">Home</a> <i>/</i> <a href="#">Blog</a> <i>/</i> With Medium Image</div>
+
+</div>
+</div><!-- end page title -->
+
+<div class="clearfix"></div>
+
+<div class="content_fullwidth">
+<div class="container">
+
+<div class="content_left">
+
+	<div class="blog_post">
+        <div class="blog_postcontent">
+        <div class="image_frame"><a href="#"><img src="http://placehold.it/1170x520" alt="" /></a></div>
+        <h3><a href="blog-post.html">Many web sites still in their infancy</a></h3>
+            <ul class="post_meta_links">
+            	<li><a href="#" class="date">27 October 2014</a></li>
+                <li class="post_by"><i>by:</i> <a href="#">Adam Harrison</a></li>
+                <li class="post_categoty"><i>in:</i> <a href="#">Web tutorials</a></li>
+                <li class="post_comments"><i>note:</i> <a href="#">18 Comments</a></li>
+            </ul>
+         <div class="clearfix"></div>
+         <div class="margin_top1"></div>
+        <p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years <a href="#">read more...</a></p>
+        </div>
+    </div><!-- /# end post -->
+
+    <div class="clearfix divider_line9"></div>
+
+    <div class="blog_post">
+        <div class="blog_postcontent">
+        <div class="image_frame"><a href="#"><img src="http://placehold.it/1170x520" alt="" /></a></div>
+        <h3><a href="blog-post.html">Letraset sheets containing lorem passages</a></h3>
+            <ul class="post_meta_links">
+            	<li><a href="#" class="date">26 October 2014</a></li>
+                <li class="post_by"><i>by:</i> <a href="#">Adam Harrison</a></li>
+                <li class="post_categoty"><i>in:</i> <a href="#">Web tutorials</a></li>
+                <li class="post_comments"><i>note:</i> <a href="#">18 Comments</a></li>
+            </ul>
+         <div class="clearfix"></div>
+         <div class="margin_top1"></div>
+        <p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years <a href="#">read more...</a></p>
+        </div>
+    </div><!-- /# end post -->
+
+    <div class="clearfix divider_line9"></div>
+
+
+    <div class="blog_post">
+        <div class="blog_postcontent">
+        <div class="video_frame"><iframe src="http://www.youtube.com/embed/YRb-xF0RW-k"></iframe></div>
+        <h3><a href="blog-post.html">Through the cites of the word in classical literature</a></h3>
+            <ul class="post_meta_links">
+            	<li><a href="#" class="date">25 October 2014</a></li>
+                <li class="post_by"><i>by:</i> <a href="#">Adam Harrison</a></li>
+                <li class="post_categoty"><i>in:</i> <a href="#">Web tutorials</a></li>
+                <li class="post_comments"><i>note:</i> <a href="#">18 Comments</a></li>
+            </ul>
+         <div class="clearfix"></div>
+         <div class="margin_top1"></div>
+        <p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years <a href="#">read more...</a></p>
+        </div>
+    </div><!-- /# end post -->
+
+    <div class="clearfix divider_line9"></div>
+
+
+    <div class="blog_post">
+        <div class="blog_postcontent">
+        <div class="image_frame"><a href="#"><img src="http://placehold.it/1170x520" alt="" /></a></div>
+        <h3><a href="blog-post.html">Making this the first true generator on the internet</a></h3>
+            <ul class="post_meta_links">
+            	<li><a href="#" class="date">24 October 2014</a></li>
+                <li class="post_by"><i>by:</i> <a href="#">Adam Harrison</a></li>
+                <li class="post_categoty"><i>in:</i> <a href="#">Web tutorials</a></li>
+                <li class="post_comments"><i>note:</i> <a href="#">18 Comments</a></li>
+            </ul>
+         <div class="clearfix"></div>
+         <div class="margin_top1"></div>
+        <p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years <a href="#">read more...</a></p>
+        </div>
+    </div><!-- /# end post -->
+
+    <div class="clearfix divider_line9"></div>
+
+
+    <div class="blog_post">
+        <div class="blog_postcontent">
+        <div class="image_frame"><a href="#"><img src="http://placehold.it/1170x520" alt="" /></a></div>
+        <h3><a href="blog-post.html">Versions have evolved over the years</a></h3>
+            <ul class="post_meta_links">
+            	<li><a href="#" class="date">23 October 2014</a></li>
+                <li class="post_by"><i>by:</i> <a href="#">Adam Harrison</a></li>
+                <li class="post_categoty"><i>in:</i> <a href="#">Web tutorials</a></li>
+                <li class="post_comments"><i>note:</i> <a href="#">18 Comments</a></li>
+            </ul>
+         <div class="clearfix"></div>
+         <div class="margin_top1"></div>
+        <p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years <a href="#">read more...</a></p>
+        </div>
+    </div><!-- /# end post -->
+
+    <div class="clearfix divider_line13 lessm"></div>
+
+	<div>
+
+    <div class="pagination">
+    <b>Page 2 of 18</b>
+    <a href="#" class="navlinks">&lt; Previous</a>
+        <a href="#" class="navlinks">1</a>
+        <a href="#" class="navlinks current">2</a>
+        <a href="#" class="navlinks">3</a>
+        <a href="#" class="navlinks">4</a>
+        <a href="#" class="navlinks">Next ></a>
+    </div><!-- /# end pagination -->
+
+    </div>
+
+
+</div><!-- end content left side -->
+
+
+<!-- right sidebar starts -->
+<div class="right_sidebar">
+
+	<div class="sidebar_widget">
+
+    	<div class="sidebar_title"><h4>Site <i>Categories</i></h4></div>
+		<ul class="arrows_list1">
+            <li><a href="#"><i class="fa fa-caret-right"></i> Economics</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Social Media</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Economics</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Online Gaming</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Entertainment</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Technology</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Make Money Online</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Photography</a></li>
+            <li><a href="#"><i class="fa fa-caret-right"></i> Web Tutorials</a></li>
+		</ul>
+
+	</div><!-- end section -->
+
+    <div class="margin_top4"></div>
+    <div class="clearfix"></div>
+
+    <div class="sidebar_widget">
+
+    	<div id="tabs">
+
+			<ul class="tabs">
+				<li class="active"><a href="#tab1">Popular</a></li>
+				<li><a href="#tab2">Recent</a></li>
+				<li class="last"><a href="#tab3">Tags</a></li>
+			</ul><!-- /# end tab links -->
+
+		<div class="tab_container">
+			<div id="tab1" class="tab_content">
+
+				<ul class="recent_posts_list">
+					<li>
+					  	<span><a href="#"><img src="http://placehold.it/50x50" alt="" /></a></span>
+						<a href="#">Publishing packag esanse web page editos</a>
+						 <i>October 13, 2014</i>
+					</li>
+					<li>
+					  	<span><a href="#"><img src="http://placehold.it/50x50" alt="" /></a></span>
+						<a href="#">Sublishing packag esanse web page editos</a>
+						 <i>October 12, 2014</i>
+					</li>
+					<li class="last">
+					  	<span><a href="#"><img src="http://placehold.it/50x50" alt="" /></a></span>
+						<a href="#">Mublishing packag esanse web page editos</a>
+						 <i>October 11, 2014</i>
+					</li>
+				</ul>
+
+			</div><!-- end popular posts -->
+
+			<div id="tab2" class="tab_content">
+				<ul class="recent_posts_list">
+
+					<li>
+					  	<span><a href="#"><img src="http://placehold.it/50x50" alt="" /></a></span>
+						<a href="#">Various versions has evolved over the years</a>
+						 <i>October 18, 2014</i>
+					</li>
+					<li>
+					  	<span><a href="#"><img src="http://placehold.it/50x50" alt="" /></a></span>
+						<a href="#">Rarious versions has evolve over the years</a>
+						 <i>October 17, 2014</i>
+					</li>
+					<li class="last">
+					  	<span><a href="#"><img src="http://placehold.it/50x50" alt="" /></a></span>
+						<a href="#">Marious versions has evolven over the years</a>
+						 <i>October 16, 2014</i>
+					</li>
+				</ul>
+
+			</div><!-- end popular articles -->
+
+			<div id="tab3" class="tab_content">
+				<ul class="tags">
+
+					<li><a href="#">2014</a></li>
+					<li><a href="#"><b>Amazing</b></a></li>
+					<li><a href="#">Animation</a></li>
+					<li><a href="#">Beautiful</a></li>
+					<li><a href="#"><b>Cartoon</b></a></li>
+					<li><a href="#">Comedy</a></li>
+					<li><a href="#"><b>Cool</b></a></li>
+					<li><a href="#">Dance</a></li>
+					<li><a href="#">Drive</a></li>
+					<li><a href="#"><b>Family</b></a></li>
+					<li><a href="#"><b>Fantasy</b></a></li>
+					<li><a href="#">News</a></li>
+					<li><a href="#"><b>Friends</b></a></li>
+					<li><a href="#">Funny</a></li>
+					<li><a href="#"><b>Games</b></a></li>
+					<li><a href="#">Love</a></li>
+					<li><a href="#"><b>Music</b></a></li>
+					<li><a href="#">Nature</a></li>
+					<li><a href="#"><b>Party</b></a></li>
+					<li><a href="#">Pictures</a></li>
+					<li><a href="#">Sports</a></li>
+					<li><a href="#"><b>Best</b></a></li>
+					<li><a href="#">Wedding</a></li>
+					<li><a href="#">Weight</a></li>
+					<li><a href="#"><b>Youtube</b></a></li>
+				</ul>
+			</div>
+
+		</div>
+
+		</div>
+
+	</div><!-- end section -->
+
+    <div class="clearfix margin_top4"></div>
+
+	<div class="sidebar_widget">
+
+        <div class="sidebar_title"><h4>Text <i>Widget</i></h4></div>
+        <p>Going to use a passage of lorem lpsum you need to be sure there anything embarrassin hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend the repeat predefined chunks as thenecessary making this the first true generator.</p>
+
+	</div><!-- end section -->
+
+    <div class="clearfix margin_top4"></div>
+
+    <div class="sidebar_widget">
+
+    	<div class="sidebar_title"><h4>Site <i>Archives</i></h4></div>
+
+		<ul class="list2">
+            <li><a href="#"><i class="fa fa-long-arrow-right"></i> October 2014</a></li>
+            <li><a href="#"><i class="fa fa-long-arrow-right"></i> September 2014</a></li>
+            <li><a href="#"><i class="fa fa-long-arrow-right"></i> August 2014</a></li>
+            <li><a href="#"><i class="fa fa-long-arrow-right"></i> July 2014</a></li>
+            <li><a href="#"><i class="fa fa-long-arrow-right"></i> June 2014</a></li>
+		</ul>
+
+	</div><!-- end section -->
+
+
+</div><!-- end right sidebar -->
+
+
+</div>
+</div><!-- end content area -->
+
+<footer>
+
+<div class="footer">
+
+    <div class="container">
+
+        <div class="left">
+
+            <h5>Get Free Consultation</h5>
+            <h6>Available 24/7</h6>
+            <h3>(888) 123-456-7890</h3>
+
+        </div><!-- end section -->
+
+        <div class="center">
+
+            <h5>Message Us Now</h5>
+            <h6>Available 24/7</h6>
+            <h3><a href="mailto:info@website.com">info@website.com</a></h3>
+
+        </div><!-- end section -->
+
+        <div class="right">
+
+            <h5>Address Location</h5>
+            <h6>2901 Marmora Road, Glassgow, Seattle, WA 98122-1090 Fax: (888) 123-456-7899 <br /> <a href="#">View Map</a></h6>
+
+        </div><!-- end section -->
+
+    </div>
+
+</div><!-- end footer -->
+
+<div class="copyrights">
+<div class="container">
+
+	<div class="one_half">Copyright © 2014 SiteName-Lawfirm. All Rights Reserved</div>
+
+    <div class="one_half last"><a href="#">Notices</a> | <a href="#">Privacy Policy</a> | <a href="#">Careers</a> | <a href="#">Site Map</a></div>
+
+
+</div>
+</div><!-- end copyrights -->
+
+</footer>
+
+
+<a href="#" class="scrollup">Scroll</a><!-- end scroll to top of the page-->
+
+
+
+
+
+</div>
+
+@endsection
