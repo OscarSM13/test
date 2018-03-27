6e3bdc071cced7c1b722f7ae97a25e05b14e07b8
Oscar Sitges <oscar.ss.mm@gmail.com>
2018-03-27 10:06:54 +0200

    Create View Contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,100 @@
 @extends('layouts.web')
 
 @section('title', 'Contact')
+
+@section('content')
+<div class="clearfix"></div>
+
+<div class="page_title2">
+<div class="container">
+	
+    <h1>Contact Us</h1>
+    <div class="pagenation">&nbsp;<a href="index.html">Home</a> <i>/</i> <a href="#">Contact</a> <i>/</i> Contact Us</div>
+    
+    
+</div>
+</div><!-- end page title --> 
+
+<div class="clearfix"></div>
+
+<div class="content_fullwidth">
+
+<div class="container">
+	
+    <div class="one_half">
+      
+        <p>Feel free to talk to our online representative at any time you please using our Live Chat system on our website or one of the below instant messaging programs.</p>
+        <br />
+        <p>Please be patient while waiting for response. (24/7 Support!) <strong>Phone General Inquiries: 1-888-123-4567-8900</strong></p>
+        <br /><br />
+        
+        <div class="cforms">
+        
+        <form action="demo-contacts.php" method="post" id="sky-form" class="sky-form">
+          <header>Contact <strong>Form</strong></header>
+          <fieldset>
+            <div class="row">
+              <section class="col col-6">
+                <label class="label">Name</label>
+                <label class="input"> <i class="icon-append icon-user"></i>
+                  <input type="text" name="name" id="name">
+                </label>
+              </section>
+              <section class="col col-6">
+                <label class="label">E-mail</label>
+                <label class="input"> <i class="icon-append icon-envelope-alt"></i>
+                  <input type="email" name="email" id="email">
+                </label>
+              </section>
+            </div>
+            <section>
+              <label class="label">Subject</label>
+              <label class="input"> <i class="icon-append icon-tag"></i>
+                <input type="text" name="subject" id="subject">
+              </label>
+            </section>
+            <section>
+              <label class="label">Message</label>
+              <label class="textarea"> <i class="icon-append icon-comment"></i>
+                <textarea rows="4" name="message" id="message"></textarea>
+              </label>
+            </section>
+            <section>
+              <label class="checkbox">
+                <input type="checkbox" name="copy" id="copy">
+                <i></i>Send a copy to my e-mail address</label>
+            </section>
+          </fieldset>
+          <footer>
+            <button type="submit" class="button">Send message</button>
+          </footer>
+          <div class="message"> <i class="icon-ok"></i>
+            <p>Your message was successfully sent!</p>
+          </div>
+        </form>
+        
+        </div>
+        
+      </div>
+      
+      <div class="one_half last">
+      
+        <div class="address_info">
+        
+          <h4>Company <strong>Address</strong></h4>
+          <ul>
+            <li> <strong>Company Name</strong><br />
+              2901 Marmora Road, Glassgow, Seattle, WA 98122-1090<br />
+              Telephone: +1 1234-567-89000<br />
+              FAX: +1 0123-4567-8900<br />
+              E-mail: <a href="mailto:mail@companyname.com">mail@companyname.com</a><br />
+              Website: <a href="index.html">www.yoursitename.com</a> </li>
+          </ul>
+        </div>
+        <div class="clearfix"></div>
+        <h4>Find the <strong>Address</strong></h4>
+        
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49208.66213843944!2d2.645735624307838!3d39.56994739481461!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12979259c61ac757%3A0xc40d5406c3d058c6!2sPalma%2C+Balearic+Islands%2C+Spain!5e0!3m2!1sen!2sus!4v1522136029872" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+</div><!-- end content area -->
+@endsection
