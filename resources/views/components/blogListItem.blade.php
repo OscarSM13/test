6f06f5c91be54370c76d4e366aefe9b82e12c198
Oscar Sitges <oscar.ss.mm@gmail.com>
2018-03-28 11:21:41 +0200

    Create migrations tables

---
+++ resources/views/components/blogListItem.blade.php
@@ -0,0 +1,24 @@
+<div class="clearfix"></div>
+
+<div class="content_fullwidth">
+<div class="container">
+
+<div class="content_left">
+
+	<div class="blog_post">
+        <div class="blog_postcontent">
+        <div class="image_frame"><a href="#"><img src="http://placehold.it/1170x520" alt="" /></a></div>
+        <h3><a href="blog-post.html">Many web sites still in their infancy</a></h3>
+            <ul class="post_meta_links">
+            	<li><a href="#" class="date">27 October 2014</a></li>
+                <li class="post_by"><i>by:</i> <a href="#">Adam Harrison</a></li>
+                <li class="post_categoty"><i>in:</i> <a href="#">Web tutorials</a></li>
+                <li class="post_comments"><i>note:</i> <a href="#">18 Comments</a></li>
+            </ul>
+         <div class="clearfix"></div>
+         <div class="margin_top1"></div>
+        <p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years <a href="#">read more...</a></p>
+        </div>
+    </div><!-- /# end post -->
+    <div class="clearfix divider_line9"></div>
+    
