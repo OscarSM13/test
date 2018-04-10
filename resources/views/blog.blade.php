6036f7b46937f03f43d70b96389281d803a9201d
Oscar Sitges <oscar.ss.mm@gmail.com>
2018-04-10 15:53:45 +0200

    Put pagination in blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -13,19 +13,17 @@
     </div>
 </div><!-- end page title -->
 
-    @include('components.blogListItem')
+<div>
+    <div class="content_fullwidth">
+        <div class="container">
+            <div class="content_left">
+                @each('components.blogListItem', $blogs, 'blog')
 
-    <div class="pagination">
-    <b>Page 2 of 18</b>
-    <a href="#" class="navlinks">&lt; Previous</a>
-        <a href="#" class="navlinks">1</a>
-        <a href="#" class="navlinks current">2</a>
-        <a href="#" class="navlinks">3</a>
-        <a href="#" class="navlinks">4</a>
-        <a href="#" class="navlinks">Next ></a>
-    </div><!-- /# end pagination -->
-</div><!-- end content left side -->
-
-@include('components.columnBlog')
+                {{ $blogs->links() }}
+            </div>
         </div>
+
+        @include('components.columnBlog')
+    </div>
+</div>
 @endsection
