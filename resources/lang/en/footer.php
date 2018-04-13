3c30a830e495b63618839171e9a99eb95b270c99
Oscar Sitges <oscar.ss.mm@gmail.com>
2018-04-13 11:12:24 +0200

    Delete comments

--- resources/lang/en/footer.php
+++ resources/lang/en/footer.php
@@ -2,25 +2,15 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
 
     'consultation' => 'Get Free Consultation',
     'avalible' => 'Avalible 24/7',
     'numberPhone'=> '(888) 123-456-7890',
-    'Message'=> 'message Us Now',
+    'message'=> 'message Us Now',
     'mail'=> 'info@website.com',
     'address1'=> 'Address Location',
     'address2'=> '2901 Marmora Road, Glassgow, Seattle, WA 98122-1090 ',
-    'Fax'=>'Fax: (888) 123-456-7899',
+    'fax'=>'Fax: (888) 123-456-7899',
     'map'=> 'View Map'
 
 ];
