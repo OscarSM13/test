54766986871e81e52ba4a7c3158d936577af0257
Oscar Sitges <oscar.ss.mm@gmail.com>
2018-04-17 09:34:57 +0200

    rename area for areas

---
+++ resources/views/areas.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.web')
+
+@section('title', 'Contact')
+@section('content')
+<div class="page_title2">
+    <div class="container">
+        
+        <h1>Practice Areas</h1>
+        <div class="pagenation">&nbsp;<a href="index.html">Home</a> <i>/</i> <a href="#">Practice Areas</a> <i>/</i> Practice Areas Style 4</div>
+            
+    </div>
+    </div><!-- end page title --> 
+    
+    <div class="clearfix"></div>
+    
+    <div class="feature_sec4 two">
+    <div class="container">
+        
+        <div id="grid-container" class="cbp-l-grid-fullScreen smallthu">
+        
+            <ul>
+                @each('components.practiceAreas', $areas, 'area')
+            </ul>
+        </div>
+            
+        <div class="cbp-l-loadMore-text">
+            <div data-href="ajax/loadMore.html" class="cbp-l-loadMore-text-link"></div>
+        </div>
+        
+    </div>
+</div>
+@endsection
