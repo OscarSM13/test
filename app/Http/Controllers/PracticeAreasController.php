45bc0f78c80f7294ff3317c44c0536ac1eceff37
Oscar Sitges <oscar.ss.mm@gmail.com>
2018-04-17 09:36:30 +0200

    controller for areas and practiceAreas

--- app/Http/Controllers/PracticeAreasController.php
+++ app/Http/Controllers/PracticeAreasController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\PracticeArea;
+use Carbon\Carbon;
 
-class HomeController extends Controller
+class PracticeAreasController extends Controller
 {
     public function index() {
         $areas = PracticeArea::all();
 
-        return view('home')->with('areas', $areas);
+        return view('areas')->with('areas', $areas);
     }
 }
