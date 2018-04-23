387ece860a939aa72353f20cabdc8b0806989185
Oscar Sitges <oscar.ss.mm@gmail.com>
2018-04-23 15:51:50 +0200

    js for form

--- resources/views/singleArea.blade.php
+++ resources/views/singleArea.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.web')
 
 @section('title', 'Contact')
+
+@push('styles')
+<link rel="stylesheet" href="{{ asset('js/form/sky-forms2.css') }}" type="text/css" media="all">
+@endpush
+
 @section('content')
 
 <div class="clearfix"></div>
+@if(!$area->getMedia()->isEmpty())
+    <div class="full_biohead" style="background:url(' {{$area->getMedia()->first()->getUrl('thumb-single-practice-area')}}')no-repeat center top" style="opacity:50">
+@else
+    <div class="full_biohead" style="background:url('http://via.placeholder.com/1800x400') no-repeat center top;">
+@endif
 
-<!--@if(!$area->getMedia()->isEmpty())-->
-<div class="full_biohead" style="background:{{ $area->image }} no-repeat center top">
-<!--@endif-->
 <div class="container">
 
 </div>
@@ -25,48 +32,48 @@
                 <p class="big_text3">{{ $area->description }}</p>
             </div>
         <div class="col-sm-4">
-            <h3>Pide presupuesto para {{ $area->title }}</h3>
-            <div class="cforms">
-        
-                <form action="demo-contacts.php" method="post" id="sky-form" class="sky-form">
-                    <br/>
-                <fieldset>
-                    <div class="row">
-                    <section class="col col-6">
-                        <label class="label">{{ trans('contact.name') }}</label>
-                        <label class="input"> <i class="icon-append icon-user"></i>
-                        <input type="text" name="name" id="name">
-                        </label>
-                    </section>
-                    <section class="col col-6">
-                        <label class="label">{{ trans('contact.mailForm') }}</label>
-                        <label class="input"> <i class="icon-append icon-envelope-alt"></i>
-                        <input type="email" name="email" id="email">
-                        </label>
-                    </section>
-                    </div>
-                    <section>
-                    <label class="label">{{ trans('contact.message') }}</label>
-                    <label class="textarea"> <i class="icon-append icon-comment"></i>
-                        <textarea rows="4" name="message" id="message"></textarea>
-                    </label>
-                    </section>
-                    <section>
-                    <label class="checkbox">
-                        <input type="checkbox" name="copy" id="copy">
-                        <i></i>{{ trans('contact.copy') }}</label>
-                    </section>
-                </fieldset>
-                <footer>
-                    <button type="submit" class="button">{{ trans('contact.send') }}</button>
-                </footer>
-                <div class="message"> <i class="icon-ok"></i>
-                    <p>{{ trans('contact.send') }}</p>
-                </div>
-                </form>
-            </div>
+                  
+                          
+                        <div class="cforms">
+                        
+                        <form action="demo-contacts.php" method="post" id="sky-form" class="sky-form">
+                            <fieldset>
+                                    <section >
+                                        <label class="label">{{ trans('contact.name') }}</label>
+                                        <label class="input"> <i class="icon-append icon-user"></i>
+                                        <input type="text" name="name" id="name">
+                                        </label>
+                                    </section>
+                                    <section >
+                                        <label class="label">{{ trans('contact.mailForm') }}</label>
+                                        <label class="input"> <i class="icon-append icon-envelope-alt"></i>
+                                        <input type="email" name="email" id="email">
+                                        </label>
+                                    </section>
+                                <section>
+                                <label class="label">{{ trans('contact.message') }}</label>
+                                <label class="textarea"> <i class="icon-append icon-comment"></i>
+                                    <textarea rows="4" name="message" id="message"></textarea>
+                                </label>
+                                </section>
+                                <section>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="copy" id="copy">
+                                    <i></i>{{ trans('contact.copy') }}</label>
+                                </section>
+                            </fieldset>
+                            <footer>
+                                <button type="submit" class="button">{{ trans('contact.send') }}</button>
+                            </footer>
+                            <div class="message"> <i class="icon-ok"></i>
+                                <p>{{ trans('contact.send') }}</p>
+                            </div>
+                        </form>
+                   
+                
         </div>
     </div>
+</div>
 </div><!-- end content area -->
 
 <div class="clearfix"></div>
